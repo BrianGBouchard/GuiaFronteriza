e558d5b362cdbdf7efdfbdedbace60907ae1db7e
BrianGBouchard <brian.g.bouchard@gmail.com>
2018-09-10 17:40:50 -0400

    8/10/18

--- GuíaFronteriza/ViewController.swift
+++ GuíaFronteriza/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
 
     @IBOutlet var control: UISegmentedControl!
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
